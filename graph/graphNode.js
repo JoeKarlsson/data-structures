7dc03c110c84b0d91df3c7e6ae9df5730cf61d68
Joe Karlsson <joe.carlson@callmejoe.net>
2016-06-21 12:39:51 -1000

    added tree and graph node implementation

---
+++ graph/graphNode.js
@@ -0,0 +1,32 @@
+ var Node = function(name) {
+  this.name = name;
+  this.neighbors = [];
+}
+
+Node.prototype.neighbors = function() {
+  return this.neighbors;
+}
+
+Node.prototype.toString = function() {
+  return this.name;
+}
+
+// and a sample graph:
+A = new Node("A")
+B = new Node("B")
+C = new Node("C")
+D = new Node("D")
+E = new Node("E")
+F = new Node("F")
+A.neighbors = [B, C]
+B.neighbors = [A, D, E]
+C.neighbors = [F]
+D.neighbors = []
+E.neighbors = []
+F.neighbors = []
+
+console.log('A', A);
+console.log('B', B);
+console.log('C', C);
+console.log('D', D);
+console.log('E', E);
--- tree/tree.js
+++ tree/tree.js
@@ -1,19 +1,49 @@
-const treeGenerator = () => {
+'use strict';
 
-  const _createNode = ( value ) => {
-    let value = value;
-    let children = [];
+const Node = function(value) {
+  this.value = value;
+  this.children = [];
+};
 
-    this.addChild = ( child ) => {
-      children.push( child );
-    };
+Node.prototype.getChildren = function() {
+  return this.getChildren;
+};
 
-    this.getChildren = () => {
-      return children;
-    }
-  }
+Node.prototype.addChild = function( child ) {
+  this.children.push( child );
+};
 
-  return {
+Node.prototype.hasChildren = function() {
+  return this.children.length > 0;
+};
 
-  }
-}
+Node.prototype.getValue = function() {
+  return this.value;
+};
+
+Node.prototype.toString = function() {
+  return this.name;
+};
+
+// and a sample graph:
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+const node6 = new Node(6);
+const node7 = new Node(7);
+const node8 = new Node(8);
+const node9 = new Node(9);
+const node10 = new Node(10);
+const node11 = new Node(11);
+const node12 = new Node(12);
+node1.addChild(node2, node7, node8);
+node2.addChild(node3, node6);
+node3.addChild(node4, node5);
+node8.addChild(node9, node12);
+node9.addChild(node10, node11);
+
+console.log('node1', node1);
+console.log('node2', node2);
+console.log('node3', node3);
