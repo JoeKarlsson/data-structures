93eadcaa91d423d43bebb12991bae6e9580f5274
Joe Karlsson <josephcarlson1@gmail.com>
2017-04-10 16:37:19 -1000

    stubbed out binary heap tests

---
+++ test/binary-heap.spec.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const BinaryHeap = require('../binary-heap/binaryHeap');
+
+const { expect } = chai;
+
+describe( 'BinaryHeap', () => {
+  let heap;
+
+  beforeEach(() => {
+    heap = new BinaryHeap((x) => x);
+    const data = [10, 3, 4, 8, 2, 9, 7, 1, 2, 6, 5];
+
+    data.forEach((el) => {
+      heap.push(el);
+    });
+  });
+
+  // describe( 'Constructor', () => {
+  //   it( 'should return a new empty binary heap', () => {
+  //     const fn = ((x) => {
+  //       return x;
+  //     });
+  //     const newHeap = new BinaryHeap(fn);
+
+  //     console.log(newHeap);
+  //     expect(newHeap).to.deep.equal({
+  //       content: [],
+  //       scoreFunction: fn,
+  //     });
+
+  //   });
+  // });
+
+
+});
